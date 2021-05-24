9bd5209a299cd5cbd83db0ad783cb6cf1d17ecaf
rayalva407 <rayalva407@gmail.com>
2021-05-23 19:19:00 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+const [loading, setLoading] = useState(true)
+const [characters, setCharacters] = useState([])
+const [currentPageUrl, setCurrentPageUrl] = useState("https://rickandmortyapi.com/api/character?page=1")
+const [nextPageUrl, setNextPageUrl] = useState()
+const [prevPageUrl, setPrevPageUrl] = useState()
+const [pages, setPages] = useState()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
     </div>
   );
 }
